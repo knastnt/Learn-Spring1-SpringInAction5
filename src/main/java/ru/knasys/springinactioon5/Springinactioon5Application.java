9a6a6bed31ef038ac9c8733580318e649f555fd8
knastnt <456kot@mail.ru>
2020-03-31 14:00:24 +1000

    init

---
+++ src/main/java/ru/knasys/springinactioon5/Springinactioon5Application.java
@@ -0,0 +1,13 @@
+package ru.knasys.springinactioon5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springinactioon5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springinactioon5Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/knasys/springinactioon5/Springinactioon5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.knasys.springinactioon5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springinactioon5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

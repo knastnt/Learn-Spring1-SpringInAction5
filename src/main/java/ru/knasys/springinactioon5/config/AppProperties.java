75b26850ba5a5b7b5a19f9a1a7dc1a316e1814d3
knastnt <456kot@mail.ru>
2020-04-10 10:57:02 +1000

    Кастомный пользовательский параметр и валидация

--- src/main/java/ru/knasys/springinactioon5/config/AppProperties.java
+++ src/main/java/ru/knasys/springinactioon5/config/AppProperties.java
@@ -3,10 +3,15 @@ package ru.knasys.springinactioon5.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Min;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "taco")
+@Validated  //Обязательно для валидации
 public class AppProperties {
+    @Min(1) //Правило валидации
     private int orderPageSize = 10;
 }

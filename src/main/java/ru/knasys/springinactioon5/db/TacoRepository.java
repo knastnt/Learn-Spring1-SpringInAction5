3ba0bf1b2000b6da7dbf74651e7ca6de6c346d2f
knastnt <456kot@mail.ru>
2020-04-20 14:24:53 +1000

    tacoes to tacos pagable and sortable tacos

--- src/main/java/ru/knasys/springinactioon5/db/TacoRepository.java
+++ src/main/java/ru/knasys/springinactioon5/db/TacoRepository.java
@@ -1,7 +1,8 @@
 package ru.knasys.springinactioon5.db;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.knasys.springinactioon5.entities.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
+public interface TacoRepository extends CrudRepository<Taco, Long>, PagingAndSortingRepository<Taco, Long> {
 }
